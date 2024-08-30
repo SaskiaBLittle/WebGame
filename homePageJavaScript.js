482b02b89290cb1dcc96ddecdbcd6de36a64f875
Saskia Brienesse-Little (1000125002) <BRIESG1@student.op.ac.nz>
2024-08-30 13:08:49 +1200

    More prizes

--- homePageJavaScript.js
+++ homePageJavaScript.js
@@ -1,4 +1,5 @@
 let count = 0;
+let level = 0;
 let autoClickerAwarded = false;
 let autoClickIntervalId;
 // Function: creates a new paragraph and appends it to the bottom of the HTML body.
@@ -22,27 +23,62 @@ function checkForPrize(){
         autoClickerAwarded = true;
         const prizeElement = document.getElementById("prize")
         prizeElement.textContent = "Auto Clicker 1 Acquired"
-        startAutoClicker();
+        autoClicker1();
     }
     if (count === 200){
         const prizeElement = document.getElementById("prize")
         prizeElement.textContent = "Auto Clicker 1 Level 2"
-        startAutoClicker2();
+        autoClicker1();
+    }
+    if (count === 400){
+        const prizeElement = document.getElementById("prize")
+        prizeElement.textContent = "Auto Clicker 1 Level 2"
+        autoClicker1();
+    }
+    if (count === 500){
+        const prizeElement = document.getElementById("prize")
+        prizeElement.textContent = "Auto Clicker 2 Acquired"
+        autoClicker2();
+    }
+    if (count === 1000){
+        const prizeElement = document.getElementById("prize")
+        prizeElement.textContent = "Auto Clicker 2 Acquired"
+        autoClicker2();
     }
 }
 
 //Figure out a way to make it so when entered the function a second time it is worth 2 clicks
-function startAutoClicker(){
-    autoClickIntervalId = setInterval(() =>{
-        document.querySelector("button").click();
-    }, 1000);
+function autoClicker1(){
+    level++;
+    if (level === 1){
+        autoClickIntervalId = setInterval(() =>{
+            document.querySelector("button").click();
+        }, 2000);
+    }
+    else if (level === 2){
+        autoClickIntervalId = setInterval(() =>{
+            document.querySelector("button").click();
+        }, 2000);
+    }
+    else if (level === 3){
+        autoClickIntervalId = setInterval(() =>{
+            document.querySelector("button").click();
+        }, 2000);
+    }
 } 
-function startAutoClicker2(){
-    autoClickIntervalId = setInterval(() =>{
-        document.querySelector("button").click();
-    }, 1000);
-} 
-
+function autoClicker2(){
+    level++;
+    if (level === 4){
+        autoClickIntervalId = setInterval(() =>{
+            document.querySelector("button").click();
+        }, 1000);
+    }
+    else if (level === 5){
+        autoClickIntervalId = setInterval(() =>{
+            document.querySelector("button").click();
+        }, 1000);
+    }
+}
 /*
     1. Get references to all the buttons on the page in an array format.
     2. Loop through all the buttons and add a click event listener to each one.
